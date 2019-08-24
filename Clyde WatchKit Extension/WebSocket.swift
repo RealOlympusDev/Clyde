4c80de5b81198796441c88f9e0695864fc51dbd2
OlympusDev <catchreuben@gmail.com>
2019-08-24 16:43:53 +1200

    Grouped by date

---
+++ Clyde WatchKit Extension/WebSocket.swift
@@ -0,0 +1,12 @@
+//
+//  WebSocket.swift
+//  Clyde WatchKit Extension
+//
+//  Created by Reuben Catchpole on 10/07/19.
+//  Copyright © 2019 Reuben Catchpole. All rights reserved.
+//
+
+import Foundation
+import Network
+
+var websocketConnection: NWConnection?
---
+++ ServerRowController.swift
@@ -0,0 +1,60 @@
+//
+//  ServerRowController.swift
+//  Discord WatchKit Extension
+//
+//  Created by Reuben on 25/05/19.
+//  Copyright © 2019 Reuben. All rights reserved.
+//
+
+import WatchKit
+
+class ServerRowController: NSObject {
+    
+    @IBOutlet weak var icon: WKInterfaceImage!
+
+     var server: Server? {
+        
+        didSet {
+            
+            guard let server = server else { return }
+            
+            guard let icon = server.icon else { return }
+        
+            let icon_url = "https://cdn.discordapp.com/icons/" + (server.id ?? "") + "/"
+        
+            let icon_image = icon_url + icon + ".png"
+            
+            print(icon_image)
+        
+            imageFromUrl(icon_image)
+            
+        }
+        
+        
+    }
+    
+    public func imageFromUrl(_ urlString: String){
+        
+        if let url = URL(string: urlString) {
+            
+            let request = URLRequest(url: url)
+            let config = URLSessionConfiguration.default
+            let session = URLSession(configuration: config)
+            
+            let task = session.dataTask(with: request, completionHandler: {(data, response, error) in
+                
+                if let imageData = data as Data? {
+                    
+                    DispatchQueue.global().async {
+                        self.icon.setImageData(imageData)
+                        
+                    }
+                }
+            });
+            
+            task.resume()
+            
+        }
+    }
+
+}
